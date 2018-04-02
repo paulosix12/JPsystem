761b5b71b985ee4ab1fdffb500c1c639693088e6
Paulosix12 <paulo_edu_12@hotmail.com>
2018-04-02 15:16:48 +0000

    Correção da model fornecedores

--- app/fornecedores.php
+++ app/fornecedores.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
 
 class fornecedores extends Model
 {
-    protected $fillable = ['fornecedores'];
-    protected $guarded = ['id'];
+    protected $fillable = ['fornecedor','cidade_for','estado_for','cep_for','insc_municipal_for','insc_estadual_for','cnpj_for','nome_responsavel_for','telefone_for','email_respon_for','endereco'];
+    protected $guarded = ['id', 'created_at', 'update_at'];
     protected $table = 'fornecedores';
-    public $timestamps = false;    
 }
+
+
